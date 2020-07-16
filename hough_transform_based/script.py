81e2f3471fd4c56863ba52e8baf1b220c6723db4
BK-Modding <kavesbteja@gmail.com>
2020-07-16 21:15:06 +0530

    Setup frame retrieval code

--- hough_transform_based/script.py
+++ hough_transform_based/script.py
@@ -7,49 +7,53 @@ import matplotlib.pyplot as plt
 
 VIDEO_PATH = "drivingvideo.mp4"
 
-cap = cv2.VideoCapture(VIDEO_PATH)
-width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+def main():    
+    cap = cv2.VideoCapture(VIDEO_PATH)
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (int(width), int(height)))
+    # fourcc = cv2.VideoWriter_fourcc(*'mp4v') # how tf do these tags even work :thonk:
+    # out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (int(width), int(height)))
 
-frame_list = []
+    frame_list = []
 
-count = 0
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    count = 0
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
 
-    if ret == True:
-        frame = np.asarray(frame)
-        # results = tfnet.return_predict(frame)
+        if ret == True:
+            frame = np.asarray(frame)
+            # results = tfnet.return_predict(frame)
 
-        frame_list.append(frame)
+            frame_list.append(frame)
 
-        if count > 5:
+            if count > 5:
+                break
+
+            count += 1
+
+            new_frame = frame
+
+            # Display the resulting frame
+            # out.write(new_frame)
+            # cv2.imshow('frame', new_frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
             break
 
-        count += 1
+    disp_frame = frame_list[0]
 
-        new_frame = frame
+    print(disp_frame)
 
-        # Display the resulting frame
-        # out.write(new_frame)
-        # cv2.imshow('frame', new_frame)
+    stencil = np.zeroes_like()
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    else:
-        break
+    # When everything done, release the capture
+    cap.release()
+    # out.release()
+    cv2.destroyAllWindows()
 
-disp_frame = frame_list[0]
-
-plt.figure(figsize=(10,10))
-plt.imshow(disp_frame[:,:,0], cmap= "gray")
-plt.show()
-
-# When everything done, release the capture
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+if __name__ == "__main__":
+    main()
